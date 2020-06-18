7058c3ec9e1ce4cfd68c2f8776ee56d6b0bf6209
adeniss00 <adeniss@live.fr>
2020-06-18 16:58:39 +0200

    Roi et modif Pion

--- Chess/src/main/java/com/chess/engine/pieces/Piece.java
+++ Chess/src/main/java/com/chess/engine/pieces/Piece.java
@@ -9,13 +9,18 @@ import com.chess.engine.board.Move;
 public abstract  class Piece{
 	protected final int piecePosition;
 	protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
-    public Piece(int piecePosition,final Alliance pieceAlliance) {
+    public Piece(final int piecePosition,final Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        this.isFirstMove=false;
     }
     public Alliance getPieceAlliance(){
         return this.pieceAlliance;
+    }
+    public boolean isFirstMove(){
+        return this.isFirstMove;
     }
     public abstract Collection<Move> calculateLegalMoves(final Board board);
 }
